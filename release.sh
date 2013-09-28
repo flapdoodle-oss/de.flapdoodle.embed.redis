8215041587ac773ff7e98525bb45056e9b77dbf9
Michael Mosmann <michael@mosmann.de>
2013-09-28 13:38:58 +0200

    next run

--- release.sh
+++ release.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 mvn release:clean
-mvn -DpreparationGoals=clean release:prepare
+#mvn -DpreparationGoals=clean release:prepare
+mvn -Darguments="-Dmaven.test.skip=true" release:prepare
 mvn -Darguments="-Dmaven.test.skip=true" release:perform
 
